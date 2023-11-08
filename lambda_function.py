ba2f64a95eb49a3e5ed964b9ca4dfb8177b9f8c4
Gabriele Armento <gabriele.armento@gmail.com>
2023-11-08 01:16:26 +0100

    Update lambda to use new openai apis

--- lambda_function.py
+++ lambda_function.py
@@ -1,23 +1,129 @@
 import json
-import os
+import time
 
-import urllib3
+from openai import OpenAI
+from openai.types.beta import Assistant, Thread
 
-RELEVANTAI_PROJECT_ID = os.environ.get("RELEVANTAI_PROJECT_ID")
-RELEVANTAI_API_URL = os.environ.get("RELEVANTAI_API_URL")
+ARIADNE_OPENAI_THREAD_ID = None
+ARIADNE_OPENAI_ASSISTANT_ID = None
+
+# The system prompt lays out the context and operational framework for Ariadne.
+ARIADNE_SYSTEM_PROMPT = """## WHO ARE YOU
+You are Ariadne. Ariadne is a state-of-the-art virtual business consultant with extensive expertise in fostering startup growth and achieving profitability.
+
+## CONTEXT
+
+Ariadne, as a core member of the TESEO team, your insights are crucial for driving the business forward. You're expected to deliver expert advice, actively engage in the team's strategic discussions, and contribute significantly to the decision-making process. Remember, you're not just repeating information; you're here to analyze, advise, and act as a catalyst for business growth. 
+
+When crafting your responses, ensure they reflect your deep understanding of TESEO's business objectives, the nuances of the startup ecosystem, and the data at hand. Your role is to parse through email correspondences, extract critical business intelligence, and leverage this information to guide the founders and shareholders. 
+
+## OPERATIONAL FRAMEWORK
+
+1. Extract the essence of each email with precision, focusing on actionable insights and data-driven advice that can be directly applied to TESEO's growth strategies. 
+2. Be proactive in your approach. If the information provided is insufficient, formulate targeted questions to elicit the necessary details that will enable you to offer informed and impactful guidance. 
+3. Evaluate each email's relevance to TESEO's objectives. If an email requires no response or action, simply acknowledge it with "NO_RESPONSE." Otherwise, respond with substantive advice or the needed acknowledgment. 
+4. As a recognized member of the founding team, maintain a tone of authority and partnership. Your responses should not only provide value but also reinforce your active role in shaping the company's trajectory. 
+5. Where possible, substantiate your advice with quantitative data. Avoid vague suggestions; instead, use precise figures and statistics to support your recommendations, underscoring their validity and enhancing the decisiveness of your advice."""
+
+# This is the template for the email prompt that Ariadne will receive.
+ARIADNE_PROMPT = """This is the latest email from {from_} to {to} (cc: {cc}) with subject "{subject}", on {date}:
+
+'''
+{body}
+'''
+
+After carefully reviewing the contents of this email, provide your reflection and advice below. If a response is not needed, indicate this with "NO_RESPONSE."""
+
+
+class Ariadne:
+    def __init__(self, openai_client: OpenAI):
+        self.prompt = ARIADNE_PROMPT.strip()
+        self.openai_client = openai_client
+        # If the thread a IDs is not provided, create it
+        self.openai_assistant_thread = (
+            self.get_openai_assistant_thread(ARIADNE_OPENAI_THREAD_ID)
+            if ARIADNE_OPENAI_THREAD_ID
+            else self.create_openai_assistant_thread()
+        )
+        # If the assistant ID is not provided, create it
+        self.openai_assistant_instance = (
+            self.get_openai_assistant_instance(ARIADNE_OPENAI_ASSISTANT_ID)
+            if ARIADNE_OPENAI_ASSISTANT_ID
+            else self.create_openai_assistant_instance()
+        )
+
+    def fill_prompt(self, email: dict) -> str:
+        """Fill in the Ariadne prompt with the provided email."""
+        get_ = lambda key, default="": email.get(key, default)
+        return self.prompt.format(
+            date=get_("date"),
+            from_=get_("from"),
+            to=get_("to"),
+            cc=get_("cc", "none"),
+            subject=get_("subject"),
+            body=get_("body"),
+        )
+
+    def create_openai_assistant_instance(self) -> Assistant:
+        return self.openai_client.beta.assistants.create(
+            name="Ariadne AI",
+            instructions=ARIADNE_SYSTEM_PROMPT,
+            tools=[{"type": "retrieval"}],
+            model="gpt-4-1106-preview",
+        )
+
+    def get_openai_assistant_instance(self, assistant_id) -> Assistant:
+        return self.openai_client.beta.assistants.retrieve(assistant_id=assistant_id)
+
+    def create_openai_assistant_thread(self) -> Thread:
+        return self.openai_client.beta.threads.create()
+
+    def get_openai_assistant_thread(self, thread_id) -> Thread:
+        return self.openai_client.beta.threads.retrieve(thread_id=thread_id)
+
+    def send_message_to_assistant_thread(self, message: str) -> str:
+        """Sends a message to the Assistant thread and returns the Assistant's response."""
+        # 1. Send a new message to the assistant thread
+        self.openai_client.beta.threads.messages.create(
+            thread_id=self.openai_assistant_thread.id, role="user", content=message
+        )
+        # 2. Start the assistant thread run
+        run = self.openai_client.beta.threads.runs.create(
+            thread_id=self.openai_assistant_thread.id,
+            assistant_id=self.openai_assistant_instance.id,
+        )
+        # 3. Wait for the assistant thread run to complete
+        while run.status != "completed":
+            run = self.openai_client.beta.threads.runs.retrieve(
+                thread_id=self.openai_assistant_thread.id, run_id=run.id
+            )
+            time.sleep(1)  # Wait 1 second before checking again
+
+        # 4. Get the assistant's response from the messages
+        messages = self.openai_client.beta.threads.messages.list(
+            thread_id=self.openai_assistant_thread.id,
+            limit=5,  # Only get the last few messages
+        )
+        answer = ""
+        for msg in messages.data:
+            # If we get to a user message, we've reached the end of the assistant's response
+            if msg.role == "user":
+                break
+            # Otherwise, add the assistant's message to the answer (`messages` is in this format: https://platform.openai.com/docs/api-reference/messages/listMessages)
+            answer += [c for c in msg.content or [] if c.type == "text"][0].text.value
+            answer += "\n\n"  # Add a newline between messages
+
+        return answer.strip()
 
 
 def lambda_handler(event, context):
     """Relay email webhook from Zapier to RelevanAI."""
 
-    # If the environment variables are not set, raise an error
-    if not RELEVANTAI_PROJECT_ID or not RELEVANTAI_API_URL:
-        raise ValueError(
-            "RELEVANTAI_PROJECT_ID and RELEVANTAI_API_URL environment variables must be set"
-        )
+    # Istanciate the OpenAI client
+    openai_client = OpenAI()
 
-    # Initialize a PoolManager instance
-    http = urllib3.PoolManager()
+    # Istanciate Ariadne
+    ariadne = Ariadne(openai_client=openai_client)
 
     # Attempt to load the event body as JSON
     try:
@@ -26,19 +132,10 @@ def lambda_handler(event, context):
         # In case the body is not valid JSON, set params to an empty object
         params = {}
 
-    # Construct the data object with params as a JSON object
-    data = {"params": params, "project": RELEVANTAI_PROJECT_ID}
-
-    # Convert the data dictionary to JSON
-    encoded_data = json.dumps(data).encode("utf-8")
-
-    # Set headers for JSON content type
-    headers = {"Content-Type": "application/json"}
-
-    # Send the data to RelevantAI and wait for the response
-    response = http.request(
-        "POST", RELEVANTAI_API_URL, body=encoded_data, headers=headers
+    # Send the email to Ariadne
+    answer = ariadne.send_message_to_assistant_thread(
+        message=ariadne.fill_prompt(email=params)
     )
 
-    # Return the decoded response
-    return json.loads(response.data.decode("utf-8"))
+    # Return the answer
+    return json.dumps({"answer": answer})
